56e9628c34cb33ffc71e01b5e989e31629936699
Susmit-aiml <susmit0730@gmail.com>
2025-06-03 23:03:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+document.getElementById("myButton1").addEventListener("click", function () {
+   window.location.href = "podcastpage/podcast.html"; // Change this to your target page
+});
+
+document.getElementById("gs1").addEventListener("click", function () {
+   window.location.href = "podcastpage/podcast.html"; // Change this to your target page
+});
+
+document.getElementById("myButton2").addEventListener("click", function () {
+   window.location.href = "resumepage/resume.html"; // Change this to your target page
+});
+
+document.getElementById("gs2").addEventListener("click", function () {
+   window.location.href = "resumepage/resume.html"; // Change this to your target page
+});
+
+document.getElementById("gs3").addEventListener("click", function () {
+   window.location.href = "storypage/story.html"; // Change this to your target page
+});
+
+document.getElementById("myButton3").addEventListener("click", function () {
+   window.location.href = "storypage/story.html"; // Change this to your target page
+});
+
+document.getElementById("gs4").addEventListener("click", function () {
+   window.location.href = "pitchpage/pitch.html"; // Change this to your target page
+});
+
+document.getElementById("myButton4").addEventListener("click", function () {
+   window.location.href = "pitchpage/pitch.html"; // Change this to your target page
+});
+
